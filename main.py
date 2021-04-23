78a034267483412ba92a3facf4bf17a584dc3702
kien1nguyen <>
2021-04-23 19:19:42 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import random
+import my_module
+
+# print (my_module.pi)
+
+random_integer = random.randint(1,100)
+print(random_integer)  # range of any numbers between 1 and 100
+
+random_float = random.random()
+print(random_float)        # print from 0.000000 to 0.99999999
+
+randomFloat = random.random() * 7
+print (randomFloat)       # print from 0.0000000 to 6.000000000
+
